2c60b299c9ce6204d959714a9b85e1a53f0b7829
Kanakam Teja Maddala <kanakamteja@gmail.com>
2024-02-05 16:45:53 -0600

    Updates to Vision Subsystem, Field Map and Additional Pipelines (#68)
    * updating vision subsystem
    
    * field map file
    
    * Automated formatting changes
    
    * #47 added manual distance calculation using april tag info
    
    * vision updates after testing
    
    * Fix typo
    
    ---------
    
    Co-authored-by: wpiformat <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: David K Turner <dkt01@users.noreply.github.com>

--- src/main/include/constants/measure_up.h
+++ src/main/include/constants/measure_up.h
@@ -27,10 +27,15 @@ namespace measure_up {
     constexpr auto backLeftWOffset = 3.25_in;
     constexpr auto backLeftLOffset = 3.25_in;
   }  // namespace swerve_offsets
+  namespace shooter_targets {
+    constexpr auto speakerTagHeight = 58_in;  // needs update
+    constexpr auto trapOpeningHeight = 36.25_in;
+  }  // namespace shooter_targets
   namespace camera_front {
     constexpr auto cameraX = 0_in;  /// @todo real mounting offsets
     constexpr auto cameraZ = 7.25_in;
-    constexpr auto cameraMountAngle = 13.5_deg;
+    constexpr auto cameraMountAngle = 29.9_deg;
+    constexpr auto cameraHeight = 28.5_in;
     constexpr auto vFov = 24.85_deg * 2;
     constexpr auto hFov = 29.8_deg * 2;
   }                         // namespace camera_front
