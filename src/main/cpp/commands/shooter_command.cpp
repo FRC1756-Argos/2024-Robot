f6495d346d088bc2780d93ebb4ec7bfb9263c8bd
Teja Maddala <kanakamteja@gmail.com>
2024-02-14 19:02:28 -0600

    merge the shoot command

---
+++ src/main/cpp/commands/shooter_command.cpp
@@ -0,0 +1,40 @@
+/// \copyright Copyright (c) Argos FRC Team 1756.
+///            Open Source Software; you can modify and/or share it under the terms of
+///            the license file in the root directory of this project.
+
+#include "commands/shooter_command.h"
+
+using namespace std::chrono_literals;
+
+ShooterCommand::ShooterCommand(ShooterSubsystem* shooter)
+    : m_pShooter{shooter} {
+  AddRequirements({m_pShooter});
+  // Use addRequirements() here to declare subsystem dependencies.
+}
+
+// Called when the command is initially scheduled.
+void ShooterCommand::Initialize() {
+  if (!(m_pShooter->ReadyToShoot())) {
+    Cancel();
+    return;
+  }
+  m_pShooter->Feed(1.0);
+}
+
+// Called repeatedly when this Command is scheduled to run
+void ShooterCommand::Execute() {
+  if (!(m_pShooter->IsNotePresent())) {
+    Cancel();
+  }
+}
+
+// Called once the command ends or is interrupted.
+void ShooterCommand::End(bool interrupted) {
+  m_pShooter->Feed(0);
+  //m_pShooter->Disable();
+}
+
+// Returns true when the command should end.
+bool ShooterCommand::IsFinished() {
+  return false;
+}
