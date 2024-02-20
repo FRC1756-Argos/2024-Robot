de52ca14e243cf619174911f2d081dc48d3ad6de
David K Turner <dkt01@users.noreply.github.com>
2024-02-19 18:24:56 -0600

    Limit current for feed and intake (#117)
    Invert 775s on intake

--- src/main/include/constants/motors.h
+++ src/main/include/constants/motors.h
@@ -135,6 +135,7 @@ namespace motorConfig {
       };
       struct feedMotor {
         constexpr static auto inverted = false;
+        constexpr static auto continuousCurrentLimit = 20_A;
         constexpr static auto neutralMode = ctre::phoenix::motorcontrol::NeutralMode::Brake;
         constexpr static auto forwardLimitSwitchSource =
             ctre::phoenix::motorcontrol::LimitSwitchSource::LimitSwitchSource_FeedbackConnector;
@@ -145,7 +146,8 @@ namespace motorConfig {
 
     namespace intake {
       struct primaryIntake {
-        constexpr static auto inverted = true;
+        constexpr static auto inverted = false;
+        constexpr static auto continuousCurrentLimit = 30_A;
         constexpr static auto neutralMode = ctre::phoenix::motorcontrol::NeutralMode::Brake;
         constexpr static auto forwardLimitSwitchSource =
             ctre::phoenix::motorcontrol::LimitSwitchSource::LimitSwitchSource_FeedbackConnector;
