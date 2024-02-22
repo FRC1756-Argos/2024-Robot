af201935bd26ff2416c8407051549bac4f554e2c
Eternal2008 <Eternal2008@users.noreply.github.com>
2024-02-21 18:52:41 -0600

    Tested subwoofer shot.

--- src/main/cpp/commands/go_to_subwoofer_position_command.cpp
+++ src/main/cpp/commands/go_to_subwoofer_position_command.cpp
@@ -13,7 +13,7 @@ GoToSubwooferPositionCommand::GoToSubwooferPositionCommand(ShooterSubsystem* sho
 
 // Called when the command is initially scheduled.
 void GoToSubwooferPositionCommand::Initialize() {
-  m_pShooter->SetAmpAndTrapMode(true);
+  m_pShooter->SetAmpAndTrapMode(false);
   m_pElevator->ElevatorMoveToHeight(measure_up::elevator::lift::subwooferHeight);
   m_pElevator->SetCarriageAngle(measure_up::elevator::carriage::subwooferAngle);
   m_pShooter->ShooterGoToSpeed(5000_rpm);
