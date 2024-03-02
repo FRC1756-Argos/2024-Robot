8251baf0c95c00b650b12ebbacdeb7c61a339a63
Eternal2008 <122846197+Eternal2008@users.noreply.github.com>
2024-03-02 12:11:09 -0600

    155 add center 2 note auto routine (#185)
    * added all that could be added to auto for 1 preloaded and center wing note, need david to confirm if we need a seperate intake command for auto's.
    
    * finished auto
    
    * fixes
    
    * Huzzah
    
    ---------
    
    Co-authored-by: Eternal2008 <Eternal2008@users.noreply.github.com>

---
+++ src/main/include/commands/autonomous/autonomous_center2_wing.h
@@ -0,0 +1,58 @@
+/// \copyright Copyright (c) Argos FRC Team 1756.
+///            Open Source Software; you can modify and/or share it under the terms of
+///            the license file in the root directory of this project.
+
+#pragma once
+
+#include <argos_lib/subsystems/swappable_controllers_subsystem.h>
+#include <frc2/command/Command.h>
+#include <frc2/command/CommandHelper.h>
+#include <frc2/command/SequentialCommandGroup.h>
+
+#include <string>
+
+#include "commands/autonomous/autonomous_command.h"
+#include "subsystems/elevator_subsystem.h"
+#include "subsystems/intake_subsystem.h"
+#include "subsystems/shooter_subsystem.h"
+#include "subsystems/simple_led_subsystem.h"
+#include "subsystems/swerve_drive_subsystem.h"
+#include "subsystems/vision_subsystem.h"
+
+class AutonomousCenter2Wing
+    : public frc2::CommandHelper<frc2::Command, AutonomousCenter2Wing>
+    , public AutonomousCommand {
+ public:
+  AutonomousCenter2Wing(IntakeSubsystem& intake,
+                        ShooterSubsystem& shooter,
+                        ElevatorSubsystem& elevator,
+                        SwerveDriveSubsystem& swerve,
+                        VisionSubsystem& vision,
+                        argos_lib::SwappableControllersSubsystem& controllers,
+                        SimpleLedSubsystem& leds);
+
+  void Initialize() override;
+
+  void Execute() override;
+
+  void End(bool interrupted) override;
+
+  bool IsFinished() override;
+
+  /**
+   * @copydoc AutonomousCommand::GetName()
+   */
+  std::string GetName() const final;
+  /**
+   * @copydoc AutonomousCommand::GetCommand()
+   */
+  frc2::Command* GetCommand() final;
+
+ private:
+  IntakeSubsystem& m_Intake;
+  ShooterSubsystem& m_Shooter;
+  ElevatorSubsystem& m_Elevator;
+  SwerveDriveSubsystem& m_Swerve;
+  VisionSubsystem& m_Vision;
+  frc2::SequentialCommandGroup m_SeqCommands;
+};
