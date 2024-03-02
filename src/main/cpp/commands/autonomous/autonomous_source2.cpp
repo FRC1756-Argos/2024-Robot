89a43dcb966e4771655ca177cdb5cd84d7b3061d
Eternal2008 <Eternal2008@users.noreply.github.com>
2024-03-02 14:54:44 -0600

    Began framework for Source 3 note auto

--- src/main/cpp/commands/autonomous/autonomous_source2.cpp
+++ src/main/cpp/commands/autonomous/autonomous_source2.cpp
@@ -2,7 +2,7 @@
 ///            Open Source Software; you can modify and/or share it under the terms of
 ///            the license file in the root directory of this project.
 
-#include "commands/autonomous/autonomous_source1.h"
+#include "commands/autonomous/autonomous_source2.h"
 
 #include <frc2/command/ParallelCommandGroup.h>
 #include <frc2/command/SequentialCommandGroup.h>
@@ -15,7 +15,7 @@
 #include "commands/shooter_command.h"
 #include "subsystems/shooter_subsystem.h"
 
-AutonomousSource1::AutonomousSource1(IntakeSubsystem& intake,
+AutonomousSource2::AutonomousSource2(IntakeSubsystem& intake,
                                      ShooterSubsystem& shooter,
                                      ElevatorSubsystem& elevator,
                                      SwerveDriveSubsystem& swerve,
@@ -29,34 +29,36 @@ AutonomousSource1::AutonomousSource1(IntakeSubsystem& intake,
     , m_Vision{vision}  //, m_ShooterCommand{ShooterCommand{*shooter}}
     , m_SeqCommands{frc2::SequentialCommandGroup{
           frc2::ParallelCommandGroup{PrimeShooterCommand{m_Shooter, m_Elevator, m_Vision, 15_ft},
-                                     DriveChoreo{m_Swerve, "Source1preload.1", true}},
+                                     DriveChoreo{m_Swerve, "Source1preload.2", true}},
           AutoAimCommand{&swerve, &shooter, &elevator, &vision, &controllers, &leds, true},
-          ShooterCommand{&m_Shooter, true}}} {}
+          ShooterCommand{&m_Shooter, true},
+          frc2::ParallelCommandGroup{DriveChoreo{m_Swerve, "Source1preload.2", true}, IntakeCommand{}},
+      }} {}
 
 // Called when the command is initially scheduled.
-void AutonomousSource1::Initialize() {
+void AutonomousSource2::Initialize() {
   m_SeqCommands.Initialize();
 }
 
 // Called repeatedly when this Command is scheduled to run
-void AutonomousSource1::Execute() {
+void AutonomousSource2::Execute() {
   m_SeqCommands.Execute();
 }
 
 // Called once the command ends or is interrupted.
-void AutonomousSource1::End(bool interrupted) {
+void AutonomousSource2::End(bool interrupted) {
   m_SeqCommands.End(interrupted);
 }
 
 // Returns true when the command should end.
-bool AutonomousSource1::IsFinished() {
+bool AutonomousSource2::IsFinished() {
   return m_SeqCommands.IsFinished();
 }
 
-std::string AutonomousSource1::GetName() const {
-  return "02. 1 Preloaded Source ";
+std::string AutonomousSource2::GetName() const {
+  return "02. 2 Preloaded Source ";
 }
 
-frc2::Command* AutonomousSource1::GetCommand() {
+frc2::Command* AutonomousSource2::GetCommand() {
   return dynamic_cast<frc2::Command*>(this);
 }
