2b4e8551d02be2a41ce8e7fb166e26633c4d92b7
David K Turner <dkt01@users.noreply.github.com>
2024-04-01 17:49:40 -0500

    Exit aiming early if aimed at start (#269)

--- src/main/include/commands/auto_aim_command.h
+++ src/main/include/commands/auto_aim_command.h
@@ -42,4 +42,34 @@ class AutoAimCommand : public frc2::CommandHelper<frc2::Command, AutoAimCommand>
   SimpleLedSubsystem* m_pLeds;
   bool m_endWhenAimed;
   argos_lib::Debouncer m_aimedDebouncer;
+
+  struct AimParams {
+    units::degree_t carriageAngle;
+    units::angular_velocity::revolutions_per_minute_t shooterSpeed;
+    units::degree_t targetAngleOffset;
+    units::degree_t cameraAngleOffset;
+  };
+
+  /// @brief Read latest vision targets from vision subsystem and generate parameters useful for aiming
+  /// @return std::nullopt if no target found, otherwise parameters for aiming to vision target
+  [[nodiscard]] std::optional<AimParams> GetAimParams();
+
+  /// @brief Calculate angle offset to aim toward vision target
+  /// @param params Aiming parameters generated from vision subsystem
+  /// @return std::nullopt if no target found, otherwise offset angle
+  [[nodiscard]] std::optional<units::degree_t> GetAdjustmentOffset(const std::optional<AimParams>& params);
+
+  /// @brief Check if robot is currently aimed at vision target
+  /// @param params Aiming parameters generated from vision subsystem
+  /// @return true indicates robot is aimed, false if not aimed or no target found
+  [[nodiscard]] bool Aimed(const std::optional<AimParams>& params);
+
+  /// @brief Aim at vision target
+  /// @param params Aiming parameters generated from vision subsystem
+  /// @return true indicates robot is aimed, false if not aimed or no target found
+  [[nodiscard]] bool Aim(const std::optional<AimParams>& params);
+
+  /// @brief Set LEDs and vibration based on aiming status
+  /// @param params Aiming parameters generated from vision subsystem
+  void SetOperatorFeedback(const std::optional<AimParams>& params);
 };
